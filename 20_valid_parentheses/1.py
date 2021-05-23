5624deb533572d859a2ca2f07531176c71a94fc2
msaidzengin <m.zengin@etu.edu.tr>
2021-05-23 18:12:00 +0300

    20

--- 20_valid_parentheses/1.py
+++ 20_valid_parentheses/1.py
@@ -1,6 +1,18 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        while "()" in s or "{}" in s or '[]' in s:
-            s = s.replace("()", "").replace('{}', "").replace('[]', "")
-        return s == ''
-    
+
+        stack = []
+        mapping = { 
+            ")": "(", 
+            "}": "{", 
+            "]": "["
+        }
+
+        for char in s:
+            if char in mapping:
+                top_element = stack.pop() if stack else '#'
+                if mapping[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+        return not stack
