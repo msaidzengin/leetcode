51d946ad4e80a1fa5c7ed6056bd57478142321af
msaidzengin <m.zengin@etu.edu.tr>
2021-06-06 13:33:44 +0300

    main

--- 49_group_anagrams/1.py
+++ 49_group_anagrams/1.py
@@ -1,6 +1,16 @@
-def groupAnagrams(self, strs):
-    d = {}
-    for w in sorted(strs):
-        key = tuple(sorted(w))
-        d[key] = d.get(key, []) + [w]
-    return d.values()
+class Solution:
+    def groupAnagrams(self, strs):
+        d = {}
+        for w in sorted(strs):
+            key = tuple(sorted(w))
+            d[key] = d.get(key, []) + [w]
+        return d.values()
+
+    def run(self):
+        strs = ["eat","tea","tan","ate","nat","bat"]
+        cvp = self.groupAnagrams(strs)
+        print(cvp)
+
+if __name__ == "__main__":
+    sol = Solution()
+    sol.run()
